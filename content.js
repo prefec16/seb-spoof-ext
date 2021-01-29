08ccc5a8b38b1fbe63c35ecbbb8fa1610e93c5b0
prefec16 <prefec16@new.edvhtl.at>
2021-01-29 18:49:14 +0100

    button creation to own method, include sebs:// in forms check

--- content.js
+++ content.js
@@ -9,38 +9,40 @@ const QUIZ_ATTEMPT_DIV = "box py-3 quizattempt";
 
     let allForms = document.getElementsByTagName("form");
     for (let form of allForms) {
-        console.log(form);
         if (form.action.startsWith(SEB_PREFIX) || form.action.startsWith(SEBS_PREFIX)) {
             let input = form.children[0];
             if (input.name === CMID) {
-                let id = input.value;
-                let requestUrl = replacePrefix(form.action) + "?" + CMID + "=" + id;
+                let requestUrl = replacePrefix(form.action) + "?" + CMID + "=" + input.value;
 
-                let launchBtn = document.createElement("button");
-                launchBtn.classList.add("btn", "btn-secondary");
-                launchBtn.style.display = "inline-block";
-                launchBtn.innerText = "Launch with seb-spoof-extension";
-                launchBtn.onclick = (evt) => {
-                    browser.runtime.sendMessage({
-                        requestUrl: requestUrl,
-                        website: location.protocol + "//" + location.host
-                    }).then((msg) => {
-                        location.reload();
-                    }, (error) => {
-                        console.error(error);
-                    });
-                };
-
-                quizAttemptDiv.prepend(launchBtn);
+                quizAttemptDiv.prepend(createButton(requestUrl));
             }
         }
     }
 })();
 
-function replacePrefix(str){
-    if(str.includes(SEB_PREFIX)){
+function createButton(requestUrl) {
+    let launchBtn = document.createElement("button");
+    launchBtn.classList.add("btn", "btn-secondary");
+    launchBtn.style.display = "inline-block";
+    launchBtn.innerText = "Launch with seb-spoof-extension";
+    launchBtn.onclick = (evt) => {
+        browser.runtime.sendMessage({
+            requestUrl: requestUrl,
+            website: location.protocol + "//" + location.host
+        }).then((msg) => {
+            location.reload();
+        }, (error) => {
+            console.error(error);
+        });
+    };
+
+    return launchBtn;
+}
+
+function replacePrefix(str) {
+    if (str.includes(SEB_PREFIX)) {
         return str.replace(SEB_PREFIX, PROTOCOL);
-    } else if(str.includes(SEBS_PREFIX)){
+    } else if (str.includes(SEBS_PREFIX)) {
         return str.replace(SEBS_PREFIX, PROTOCOL);
     } else {
         return str;

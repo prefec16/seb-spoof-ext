62baaed2eb562a32a229ba3a4e15d999b186cc94
prefec16 <prefec16@new.edvhtl.at>
2021-01-29 17:09:16 +0100

    remove listener

--- background.js
+++ background.js
@@ -4,6 +4,8 @@ const COMPANION_LOCATION = "http://localhost:9999";
 
 browser.runtime.onMessage.addListener(async (msg) => {
     if (msg.requestUrl !== undefined) {
+        browser.webRequest.onBeforeSendHeaders.removeListener(headerListener);
+
         let configRequest = await fetch(msg.requestUrl, {
             credentials: "include"
         });
@@ -22,40 +24,43 @@ browser.runtime.onMessage.addListener(async (msg) => {
             if (keyRequest.status === 200) {
                 let keys = await keyRequest.json();
                 console.log(keys);
-                browser.webRequest.onBeforeSendHeaders.addListener((event) => {
-                    let url = event.url;
-                    console.log("requesting " + url);
-
-                    return new Promise(((resolve, reject) => {
-                        fetch(COMPANION_LOCATION + "/urlhashes", {
-                            method: "POST",
-                            body: JSON.stringify({
-                                url: url,
-                                ...keys
-                            })
-                        }).then(response => {
-                            if (response.status === 200) {
-                                response.json().then(json => {
-                                    new Map([
-                                        [REQUEST_HASH_HEADER, json["requestHash"]],
-                                        [CONFIG_KEY_HEADER, json["configKeyHash"]],
-                                        ["Sec-Fetch-Site", "none"],
-                                        ["Sec-Fetch-Mode", "navigate"],
-                                        ["Sec-Fetch-User", "?1"],
-                                        ["Sec-Fetch-Dest", "document"]
-                                    ]).forEach((value, key) => {
-                                        event.requestHeaders.push({name: key, value: value});
-                                    });
-
-                                    resolve();
-                                });
-                            }
-                        })
-                    })).then(value => {
-                        return {requestHeaders: event.requestHeaders};
-                    });
-                }, {urls: [msg.website + "/*"]}, ["requestHeaders", "blocking"]);
+                browser.webRequest.onBeforeSendHeaders.addListener(e => headerListener(e, keys), {urls: [msg.website + "/*"]}, ["requestHeaders", "blocking"]);
             }
         }
     }
 });
+
+function headerListener(event, keys) {
+    let url = event.url;
+    console.log("requesting " + url);
+
+    return new Promise(((resolve, reject) => {
+        fetch(COMPANION_LOCATION + "/urlhashes", {
+            method: "POST",
+            body: JSON.stringify({
+                url: url,
+                ...keys
+            })
+        }).then(response => {
+            if (response.status === 200) {
+                response.json().then(json => {
+                    new Map([
+                        [REQUEST_HASH_HEADER, json["requestHash"]],
+                        [CONFIG_KEY_HEADER, json["configKeyHash"]],
+                        ["Sec-Fetch-Site", "none"],
+                        ["Sec-Fetch-Mode", "navigate"],
+                        ["Sec-Fetch-User", "?1"],
+                        ["Sec-Fetch-Dest", "document"]
+                    ]).forEach((value, key) => {
+                        event.requestHeaders.push({name: key, value: value});
+                    });
+
+                    resolve();
+                });
+            }
+        })
+    })).then(value => {
+        return {requestHeaders: event.requestHeaders};
+    });
+}
+
